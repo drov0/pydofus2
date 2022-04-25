b672f93d410beca8da4a000737acda0749fccf07
unknown <majdoub.khalid@gmail.com>
2022-04-25 22:40:50 +0200

    added two missing classes from the last commit"

---
+++ com/ankamagames/dofus/network/messages/game/context/roleplay/MapChangeFailedMessage.py
@@ -0,0 +1,9 @@
+from com.ankamagames.jerakine.network.NetworkMessage import NetworkMessage
+
+
+class MapChangeFailedMessage(NetworkMessage):
+    mapId: int
+
+    def init(self, mapId):
+        self.mapId = mapId
+        super().__init__()
---
+++ com/ankamagames/jerakine/entities/interfaces/IMovable.py
@@ -0,0 +1,24 @@
+from types import FunctionType
+from com.ankamagames.jerakine.entities.interfaces.IEntity import IEntity
+from com.ankamagames.jerakine.types.positions.MapPoint import MapPoint
+from com.ankamagames.jerakine.types.positions.MovementPath import MovementPath
+
+
+class IMovable(IEntity):
+    @property
+    def isMoving(self) -> bool:
+        raise NotImplementedError()
+
+    def move(
+        self,
+        param1: MovementPath,
+        param2: FunctionType = None,
+        param3=None,
+    ) -> None:
+        raise NotImplementedError()
+
+    def jump(self, param1: MapPoint) -> None:
+        raise NotImplementedError()
+
+    def stop(self, param1: bool = False) -> None:
+        raise NotImplementedError()

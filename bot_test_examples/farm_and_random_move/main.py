763a38c83d4cbb25b8c0d116c4a6ed84ad63a92d
Khalid MAJDOUB <majdoub.khalid@gmail.com>
2022-04-27 03:28:34 +0200

    Update main.py

--- bot_test_examples/farm_and_random_move/main.py
+++ bot_test_examples/farm_and_random_move/main.py
@@ -3,13 +3,10 @@ from pyd2bot.frames.BotGameApproach import BotGameApproach
 from pyd2bot.Bot import Bot
 from com.ankamagames.jerakine.logger.Logger import Logger
 import threading
-
 logger = Logger(__name__)
 
-newMap = threading.Event()
-
-
 if __name__ == "__main__":
-    bot = Bot("grinder")
+    botName  = "grinder"
+    bot = Bot(botName)
     bot._worker.addFrame(BotFarmFrame())
     bot.start()

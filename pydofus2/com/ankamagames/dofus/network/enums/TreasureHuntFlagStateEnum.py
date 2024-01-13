183b160438c345f211adf7b2bf8ca5135c6d4ccf
Hexalgo <hexalgo@gmail.com>
2024-01-13 08:12:17 +0100

    add type for unsubmited flags

--- pydofus2/com/ankamagames/dofus/network/enums/TreasureHuntFlagStateEnum.py
+++ pydofus2/com/ankamagames/dofus/network/enums/TreasureHuntFlagStateEnum.py
@@ -2,6 +2,7 @@ class TreasureHuntFlagStateEnum:
     TREASURE_HUNT_FLAG_STATE_UNKNOWN = 0
     TREASURE_HUNT_FLAG_STATE_OK = 1
     TREASURE_HUNT_FLAG_STATE_WRONG = 2
+    TREASURE_HUNT_FLAG_STATE_UNSUBMITTED = -1
     
     def to_string(state):
         if state == TreasureHuntFlagStateEnum.TREASURE_HUNT_FLAG_STATE_UNKNOWN:
